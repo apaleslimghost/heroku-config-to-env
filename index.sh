abe88e2b25d4ef8d7132d04cfa382b8b005117ab
Matt Brennan <mattyb1000@gmail.com>
2016-04-05 17:43:23 +0100

    add support for ignored vars

--- index.sh
+++ index.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+ignore=("thisstringisnevergoingtoappearlolbutwedontwanttofilterouteverythingwhichiswhatanemptyarraywoulddo")
+
+while getopts ':i:' opt; do
+		case $opt in
+				i) ignore+=("$OPTARG")
+		esac
+done
+
+shift $((OPTIND -1))
+
+function join { local IFS="$1"; shift; echo "$*"; }
+
 readonly APP_NAME="$1"
 readonly OUT_FILE="$2"
 
@@ -8,6 +20,7 @@ mkdir -p "$(dirname "$OUT_FILE")"
 heroku config --app "$APP_NAME" |\
 		tail +2 |\
 		sed 's/: */=/;  s/^/export /' |\
+		grep -vE "($(join '|' ${ignore[@]}))" |\
 		cat <(echo '#!/bin/sh') - > "$OUT_FILE"
 
 chmod +x $OUT_FILE
