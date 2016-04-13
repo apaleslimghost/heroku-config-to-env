8bf54054d64663e5f5845eff3d3f474963289367
Matt Brennan <mattyb1000@gmail.com>
2016-04-13 10:14:16 +0100

    add usage, make outfile optional

--- index.sh
+++ index.sh
@@ -2,10 +2,33 @@
 
 ignore=("thisstringisnevergoingtoappearlolbutwedontwanttofilterouteverythingwhichiswhatanemptyarraywoulddo")
 
-while getopts ':i:l:' opt; do
+usage() {
+	if [ "$1" != "" ]; then
+		echo "$1"
+		echo "────────────────"
+	fi
+
+	cat <<EOF
+Usage: $(basename $0) heroku-app-name [path/to/env.sh]
+
+path/to/env.sh is optional. if it's provided, the file will be written
+and made executable. if it's blank or '-', it's written to stdout.
+
+Options:
+	-i PATTERN	ignore any config var matching PATTERN. can be used multiple times
+	-l VAR=val	add an entry setting VAR to val. use with -i to prevent duplicates
+	-h		show this help
+EOF
+
+	exit $([ "$1" == "" ] ; echo $?)
+}
+
+while getopts ':i:l:h' opt; do
 		case $opt in
 				i) ignore+=("$OPTARG") ;;
-				l) localv+=("$OPTARG")
+				l) localv+=("$OPTARG") ;;
+				h) usage ;;
+				?) usage "Unknown option $OPTARG"
 		esac
 done
 
@@ -14,7 +37,15 @@ shift $((OPTIND -1))
 function join { local IFS="$1"; shift; echo "$*"; }
 
 readonly APP_NAME="$1"
-readonly OUT_FILE="$2"
+OUT_FILE="$2"
+
+if [ "$APP_NAME" == "" ]; then
+	usage "App name missing"
+fi
+
+if [ "$OUT_FILE" == "" ] || [ "$OUT_FILE" == "-" ] ; then
+	OUT_FILE="/dev/stdout"
+fi
 
 mkdir -p "$(dirname "$OUT_FILE")"
 
@@ -26,5 +57,6 @@ heroku config --app "$APP_NAME" |\
 		sed 's/^/export /' |\
 		cat <(echo '#!/bin/sh') - > "$OUT_FILE"
 
-chmod +x $OUT_FILE
-
+if [ "$OUT_FILE" != "/dev/stdout" ]; then
+	chmod +x $OUT_FILE
+fi
